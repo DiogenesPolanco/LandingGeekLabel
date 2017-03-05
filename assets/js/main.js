738f5817e8d85a9c4263484d78f21dc5a7d8b99f
Diogenes Polanco <dpolanco@localhost.localdomain>
2017-03-05 17:59:35 -0400

    firts commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+	$('a[href*=#]').on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 500, 'linear');
+	});
+});
+ 
